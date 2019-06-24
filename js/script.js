e9fa71621b061c0871188f0d376b84f2f28e6684
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2019-06-24 16:37:37 -0300

    Projeto da disciplina de Laboratório Finalizado com sucesso!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-//Java script file 
+//Validação do formulário: 
+
+function validForm(){
+    var nome = formContato.nome.value;            
+    var email = formContato.email.value;            
+    var assunto = formContato.assunto.value;            
+    var message = formContato.message.value;            
+
+    //Validações: 
+    if(nome == "")
+    {
+        alert("Campo nome é obrigatório");
+        formContato.nome.focus();
+        return false;
+    }
+    if(email == "")
+    {
+        alert("Campo email é obrigatório");
+        formContato.email.focus();
+        return false;
+    }
+    if(assunto == "")
+    {
+        alert("Campo assunto é obrigatório");
+        formContato.assunto.focus();
+        return false;
+    }
+    if(message == "")
+    {
+        alert("Campo mensagem é obrigatório");
+        formContato.message.focus();
+        return false;
+    }
+
+    if(nome != "" && email != "" && assunto != "" && message != ""){
+        alert("Mensagem enviada com sucesso, entraremos em contato assim que possível.");
+        return true;
+    }
+}
